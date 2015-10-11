ece68a2f70d6b1e6c3e9208886973af60949cb9d
Ben Centra <bencentra@users.noreply.github.com>
2015-10-10 22:09:20 -0400

    Displaying date and time correctly

--- src/main.c
+++ src/main.c
@@ -9,120 +9,81 @@
 #define KEY_CONDITIONS 1
   
 static Window *s_main_window;
-static TextLayer *s_time_label, *s_day_label, *s_num_label;
-static Layer *s_date_layer, *s_time_layer;
+static TextLayer *s_time_layer, *s_date_layer, *s_weather_layer;
 static GFont *s_font_leco_lg, *s_font_leco_sm;
-static char s_time_buffer[10], s_num_buffer[4], s_day_buffer[6];
-  
+static char s_time_buffer[10], s_num_buffer[4], s_day_buffer[6], s_date_buffer[12], s_temp_buffer[8], s_cond_buffer[32], s_weather_buffer[32];
+
 /*
 *  Watchface
 */
 
-// static void update_time() {
-//   // Get time struct
-//   time_t tmp = time(NULL);
-//   struct tm *tick_time = localtime(&tmp);
-  
-//   // Create long-lived buffer
-//   static char buffer[] = "00:00";
-  
-//   // Set the time
-//   if(clock_is_24h_style() == true) {
-//     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-//   } 
-//   else {
-//     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-//   }
-  
-//   // Display the time
-//   text_layer_set_text(s_time_layer, buffer);
-// }
+static void update_time() {
+  // Get time struct
+  time_t tmp = time(NULL);
+  struct tm *tick_time = localtime(&tmp);
 
-static void date_update_proc(Layer *layer, GContext *ctx) {
+  // Set the time
+  if(clock_is_24h_style() == true) {
+    strftime(s_time_buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } 
+  else {
+    strftime(s_time_buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  // Display the time
+  text_layer_set_text(s_time_layer, s_time_buffer);
+}
+
+static void update_date() {
   // Get time struct
   time_t tmp = time(NULL);
   struct tm *t = localtime(&tmp);
   
   // Set the date
   strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
-  text_layer_set_text(s_day_label, s_day_buffer);
   strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
-  text_layer_set_text(s_num_label, s_num_buffer);
-}
-
-static void time_update_proc(Layer *layer, GContext *ctx) {
-//   update_time();
-  // Get time struct
-  time_t tmp = time(NULL);
-  struct tm *tick_time = localtime(&tmp);
+  snprintf(s_date_buffer, sizeof(s_date_buffer), "%s %s", s_day_buffer, s_num_buffer);
   
-  if(clock_is_24h_style() == true) {
-    strftime(s_time_buffer, sizeof(s_time_buffer), "%H:%M", tick_time);
-  }
-  else {
-    strftime(s_time_buffer, sizeof(s_time_buffer), "%I:%M", tick_time);
-  }
-  text_layer_set_text(s_time_label, s_time_buffer);
+  // Display the date
+  text_layer_set_text(s_date_layer, s_date_buffer);
 }
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
   
   // Create fonts
   s_font_leco_lg = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_LECO_BOLD_LETTERS_42));
   s_font_leco_sm = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_LECO_BOLD_LETTERS_18));
   
-  // Create day label
-  s_day_label = text_layer_create(GRect(0, 42, 70, 42));
-  text_layer_set_text(s_day_label, s_day_buffer);
-  text_layer_set_background_color(s_day_label, GColorClear);
-  text_layer_set_text_color(s_day_label, GColorBlack);
-  text_layer_set_font(s_day_label, s_font_leco_sm);
-  text_layer_set_text_alignment(s_day_label, GTextAlignmentRight);
-  
-  // Create num label
-  s_num_label = text_layer_create(GRect(74, 42, 70, 42));
-  text_layer_set_text(s_num_label, s_num_buffer);
-  text_layer_set_background_color(s_num_label, GColorClear);
-  text_layer_set_text_color(s_num_label, GColorBlack);
-  text_layer_set_font(s_num_label, s_font_leco_sm);
-  text_layer_set_text_alignment(s_num_label, GTextAlignmentLeft);
-  
   // Create the date layer
-  s_date_layer = layer_create(bounds);
-  layer_set_update_proc(s_date_layer, date_update_proc);
-  layer_add_child(window_layer, s_date_layer);
-  layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
-  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
-  
-  // Create the time label
-  s_time_label = text_layer_create(GRect(0, 54, 144, 168));
-  text_layer_set_background_color(s_time_label, GColorClear);
-  text_layer_set_text_color(s_time_label, GColorBlack);
-  text_layer_set_font(s_time_label, s_font_leco_lg);
-  text_layer_set_text_alignment(s_time_label, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_time_label));
+  s_date_layer = text_layer_create(GRect(0, 36, 144, 168));
+  text_layer_set_text(s_date_layer, s_date_buffer);
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, s_font_leco_sm);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
   
   // Create the time layer
-  s_time_layer = layer_create(bounds);
-  layer_set_update_proc(s_time_layer, time_update_proc);
-  layer_add_child(window_layer, s_time_layer);
+  s_time_layer = text_layer_create(GRect(0, 54, 144, 168));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_font(s_time_layer, s_font_leco_lg);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy everything!
-//   text_layer_destroy(s_time_layer);
-  layer_destroy(s_time_layer);
-  layer_destroy(s_date_layer);
-  text_layer_destroy(s_day_label);
-  text_layer_destroy(s_num_label);
-  text_layer_destroy(s_time_label);
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+  fonts_unload_custom_font(s_font_leco_lg);
+  fonts_unload_custom_font(s_font_leco_sm);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(window_get_root_layer(s_main_window));
-//   update_time();
+  update_time();
+  update_date();
 }
 
 /*
@@ -164,7 +125,8 @@ static void init() {
   // Regiseter AppMessage callbacks
   
   // Display initial time
-//   update_time();
+  update_time();
+  update_date();
 }
 
 static void deinit() {
