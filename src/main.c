95e34bdc6a5274512096ea77be2b0c3d5c7ff8b4
Ben Centra <bencentra@users.noreply.github.com>
2015-10-10 20:24:20 -0400

    Displaying date!

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
 /*
-*  My first watchface. 
+*  My first watch face for Pebble Time.
 *  Influenced by Kiezel's "Essential" and "Simply Time" watchfaces.
 */
   
@@ -9,30 +9,73 @@
 #define KEY_CONDITIONS 1
   
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static TextLayer *s_time_layer, *s_day_label, *s_num_label;
+static Layer *s_date_layer;
 static GFont *s_font_leco_lg, *s_font_leco_sm;
+static char s_num_buffer[4], s_day_buffer[6];
   
 /*
 *  Watchface
 */
 
+static void date_update_proc(Layer *layer, GContext *ctx) {
+  // Get time struct
+  time_t tmp = time(NULL);
+  struct tm *t = localtime(&tmp);
+  
+  // Set the date
+  strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
+  text_layer_set_text(s_day_label, s_day_buffer);
+  strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
+  text_layer_set_text(s_num_label, s_num_buffer);
+}
+
 static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
   // Create fonts
-  s_font_leco_lg = fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS);
-  s_font_leco_sm = fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS);
+  s_font_leco_lg = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_LECO_BOLD_LETTERS_42));
+  s_font_leco_sm = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_LECO_BOLD_LETTERS_18));
+  
+  // Create day label
+  s_day_label = text_layer_create(GRect(0, 42, 70, 42));
+  text_layer_set_text(s_day_label, s_day_buffer);
+  text_layer_set_background_color(s_day_label, GColorClear);
+  text_layer_set_text_color(s_day_label, GColorBlack);
+  text_layer_set_font(s_day_label, s_font_leco_sm);
+  text_layer_set_text_alignment(s_day_label, GTextAlignmentRight);
+  
+  // Create num label
+  s_num_label = text_layer_create(GRect(74, 42, 70, 42));
+  text_layer_set_text(s_num_label, s_num_buffer);
+  text_layer_set_background_color(s_num_label, GColorClear);
+  text_layer_set_text_color(s_num_label, GColorBlack);
+  text_layer_set_font(s_num_label, s_font_leco_sm);
+  text_layer_set_text_alignment(s_num_label, GTextAlignmentLeft);
+  
+  // Create the date layer
+  s_date_layer = layer_create(bounds);
+  layer_set_update_proc(s_date_layer, date_update_proc);
+  layer_add_child(window_layer, s_date_layer);
+  layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
+  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
   
   // Create the time layer
-  s_time_layer = text_layer_create(GRect(0, 0, 144, 168));
+  s_time_layer = text_layer_create(GRect(0, 54, 144, 168));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_font(s_time_layer, s_font_leco_lg);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy everything!
   text_layer_destroy(s_time_layer);
+  layer_destroy(s_date_layer);
+  text_layer_destroy(s_day_label);
+  text_layer_destroy(s_num_label);
 }
 
 static void update_time() {
@@ -56,6 +99,7 @@ static void update_time() {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+//   layer_mark_dirty(window_get_root_layer(window));
   update_time();
 }
 
@@ -102,6 +146,7 @@ static void init() {
 }
 
 static void deinit() {
+  tick_timer_service_unsubscribe();
   window_destroy(s_main_window);
 }
 
